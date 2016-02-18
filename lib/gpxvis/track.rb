98092d8f5f9f60728ffc0dde1a3ccbd21d8c410c
aj esler <andrew.esler@powershop.co.nz>
2016-02-18 17:36:23 +1300

    Add moving duration based on < 2m between points

--- lib/gpxvis/track.rb
+++ lib/gpxvis/track.rb
@@ -29,8 +29,20 @@ module Gpxvis
 
     def duration
       @duration ||= points.each_cons(2).map do |p1, p2|
-        delta = p2.time.to_time.to_i - p1.time.to_time.to_i
+        p2.time.to_time.to_i - p1.time.to_time.to_i
       end.reduce(:+)
+    end
+
+    def moving_duration
+      @moving_duration ||= moving_points.map do |p1, p2|
+        p2.time.to_time.to_i - p1.time.to_time.to_i
+      end.reduce(:+)
+    end
+
+    def moving_points
+      points.each_cons(2).select do |p1, p2|
+        p1.distance_from(p2) > 2.0
+      end
     end
 
     # avg 3 in front, 3 behind for elevation
@@ -50,6 +62,8 @@ module Gpxvis
         Stat.new(:length, length, "meters"),
         Stat.new(:duration, duration, "seconds"),
         Stat.new(:duration_human, ChronicDuration.output(duration), nil),
+        Stat.new(:moving_duration, moving_duration, "seconds"),
+        Stat.new(:moving_duration_human, ChronicDuration.output(moving_duration), nil),
         Stat.new(:point_count, points.count, nil)
       ]
     end
